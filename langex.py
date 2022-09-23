5afb9c3aaa40d3f54948165611814c03b65765ce
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2022-09-23 20:28:45 +0300

    Added cmd args support

--- langex.py
+++ langex.py
@@ -3,44 +3,54 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from langex.match import generate_matches
 from langex.utils import *
+import argparse
+import sys
 
 
 def main():
+    parser = argparse.ArgumentParser(description='MSLU langex sort utility')
+    parser.add_argument('-of', '--offline', action='store_true',
+                        help="Do not download the files from network, open existing")
+    args = parser.parse_args()
+
     log("Langex started.")
 
-    scope = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        'credentials.json', scope)
+    if not args.offline:
+        scope = ['https://spreadsheets.google.com/feeds',
+                 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            'credentials.json', scope)
 
-    sheets = {
-        "eng": {
-            "id": "1p7tMW5M9Ibedfv-FD7lw9lUC7qUd6udrPOLshnur3xk",
-            "spreadsheet": []
+        sheets = {
+            "eng": {
+                "id": "1p7tMW5M9Ibedfv-FD7lw9lUC7qUd6udrPOLshnur3xk",
+                "spreadsheet": []
+            }
         }
-    }
 
-    log("Logging in google services...", end=" ")
-    client = gspread.authorize(credentials)
-    done()
+        log("Logging in google services...", end=" ")
+        client = gspread.authorize(credentials)
+        done()
 
-    log("Downloading tables...", end=" ")
-    for lng in sheets.keys():
-        sheets[lng]["spreadsheet"] = client.open_by_key(sheets[lng]["id"])
-    done()
+        log("Downloading tables...", end=" ")
+        for lng in sheets.keys():
+            sheets[lng]["spreadsheet"] = client.open_by_key(sheets[lng]["id"])
+        done()
 
-    log("Writing CSV...", end=" ")
-    for lng in sheets.keys():
-        spreadsheet = sheets[lng]["spreadsheet"].worksheets()[0]
-        filename = lng + '-worksheet-' + \
-            str(datetime.date.today()) + '.csv'
-        with open(filename, 'w', encoding="utf-8", newline="") as f:
-            writer = csv.writer(f, delimiter=";")
-            writer.writerows(remove_column_from_matrix(
-                spreadsheet.get_all_values(), 0))
-            sheets[lng]["spreadsheet"] = remove_column_from_matrix(
-                spreadsheet.get_all_values(), 0)
-    done()
+        log("Writing CSV...", end=" ")
+        for lng in sheets.keys():
+            spreadsheet = sheets[lng]["spreadsheet"].worksheets()[0]
+            filename = lng + '-worksheet-' + \
+                str(datetime.date.today()) + '.csv'
+            with open(filename, 'w', encoding="utf-8", newline="") as f:
+                writer = csv.writer(f, delimiter=";")
+                writer.writerows(remove_column_from_matrix(
+                    spreadsheet.get_all_values(), 0))
+                sheets[lng]["spreadsheet"] = remove_column_from_matrix(
+                    spreadsheet.get_all_values(), 0)
+        done()
+    elif args.offline == True:
+        pass
 
     log("Generating matches...", end=" ")
     with open("matches-" + str(datetime.date.today()) + '.csv', 'w', encoding="utf-8", newline="") as f:
--- langex/match.py
+++ langex/match.py
@@ -4,30 +4,7 @@ from difflib import SequenceMatcher
 
 users = []
 matches = []
-
-
-class User:
-    class Hobby:
-        name = ""
-        value = ""
-        weight = 1.0
-
-    sheet_id = ""  # lang_name + user's id in a worksheet, eng-1, chn-15, etc.
-    name = ""
-    lng_knows = []
-    lng_want_to_know = []
-
-    hobbies = []
-
-    def __init__(self, table_row, sheet_id):
-        self.sheet_id = sheet_id
-        self.name = table_row[0]
-        self.lng_knows = [i.strip().lower() for i in table_row[2].split(",")]
-        self.lng_want_to_know = [i.strip().lower()
-                                 for i in table_row[3].split(",")]
-
-        table_row = [table_row[1], *table_row[4:]]
-        self.hobbies = table_row
+column_titles = []
 
 
 class UserMatch:
@@ -44,20 +21,71 @@ class UserMatch:
                 self.user2 == obj.user2 and self.percent == obj.percent)
 
 
+class Hobby:
+    name = ""
+    value = ""
+    weight = 1.0
+
+    def __init__(self, name, value, weight=1.0):
+        self.name = name
+        self.value = ",".join(sorted([i.strip() for i in value.split(",")]))
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name}({self.weight}): {self.value}"
+
+
+class User:
+    sheet_id = ""  # lang_name + user's id in a worksheet, eng-1, chn-15, etc.
+    name = ""
+    lng_knows = []
+    lng_want_to_know = []
+
+    hobbies = {}
+
+    def __init__(self, table_row, sheet_id):
+        global column_titles
+
+        self.sheet_id = sheet_id
+        self.name = table_row[0]
+        self.lng_knows = [i.strip().lower() for i in table_row[2].split(",")]
+        self.lng_want_to_know = [i.strip().lower()
+                                 for i in table_row[3].split(",")]
+
+        for i, cell in enumerate(table_row):
+            if i in [i for i in range(4)]: 
+                continue
+            self.hobbies[column_titles[i]] = Hobby(column_titles[i], cell)
+
+    def match_with(self, user2):
+        average = 0
+        for hobby_name in self.hobbies.keys():
+            hobby = self.hobbies[hobby_name]
+
+            average += SequenceMatcher(a=hobby.value, 
+                b=user2.hobbies[hobby.name].value).ratio()*100
+
+        return UserMatch(self, user2, int(average / len(self.hobbies)))
+
+
 def generate_matches(sheets):
     for key in sheets.keys():
+        
+        global column_titles
+        column_titles = sheets[key]["spreadsheet"][0]
+
         counter = 1
-        for user_table_row in sheets[key]["spreadsheet"]:
+        for user_table_row in sheets[key]["spreadsheet"][1:]:
             users.append(User(user_table_row, f"{key}-{counter}"))
             counter += 1
 
-    matches.append(UserMatch(users[0], users[1], 100))
-    matches.append(UserMatch(users[1], users[0], 100))
+    matches.append(users[0].match_with(users[1]))
 
     result = [("user 1", "user 2", "match, %")]
 
     sorted_matches = []
     for mtch in matches:
+        print(mtch.percent)
         if not mtch in sorted_matches and mtch.percent >= 100/3:
             sorted_matches.append(mtch)
 
