e39756ebca8a06f4ada56fbabaab2383c3b8de8c
Alexander Rusakevich <mr.alexander.rusakevich@gmail.com>
2022-10-09 15:35:36 +0300

    Added matches sync

--- langex.py
+++ langex.py
@@ -6,6 +6,7 @@ from langex.utils import *
 import argparse
 import re
 import json
+import time
 from os import listdir
 from os.path import isfile
 
@@ -17,10 +18,17 @@ def main():
     parser.add_argument('-nofs', '--no-offline-strip', action="store_true", help="Prevent from stripping the first column of CSV\
         in offline mode")
     args = parser.parse_args()
+    client = None
 
     log("Langex started.")
 
     sheets = json.load(open("./sheets_cfg.json", "r", encoding="utf-8"))
+
+    sheets_id = json.load(open("./sheets_id.json", "r", encoding="utf-8"))
+    for name in sheets_id.keys():
+        if name == "__matches":
+            continue
+        sheets[name]["id"] = sheets_id[name]
 
     if not args.offline:
         scope = ['https://spreadsheets.google.com/feeds',
@@ -77,27 +85,47 @@ def main():
 
                 log(f'Read "{filepath}"')
 
+    if args.offline == False:
+        log("Getting previous matches...", end=" ")
+        table = client.open_by_key(sheets_id["__matches"]).worksheets()[0]
+        tb_values = table.get_all_values()
+
+        with open("./matches.csv", 'w', encoding="utf-8", newline="") as f:
+            writer = csv.writer(f, delimiter=",")
+            writer.writerows(tb_values)
+        done()
+
     log("Generating matches...", end=" ")
     prev_matches_rows = []
     with open("matches.csv", 'r', encoding="utf-8") as f:
         for line in f.readlines()[1:]:
-            line = re.sub(r"\s*;\s*", ";", line.strip())
+            line = re.sub(r"\s+;\s+", ";", line.strip())
+            line = re.sub(r"\s+,\s+", ",", line)
             prev_matches_rows.append(line)
 
     with open("matches.csv", 'w', encoding="utf-8", newline="") as f:
-        writer = csv.writer(f, delimiter=";")
+        writer = csv.writer(f, delimiter=",")
+
+        gen_start_time = time.time()
         matches = generate_matches(sheets)
+        gen_end_time = time.time() - gen_start_time
+        print("Done in", gen_end_time, "sec.")
 
         for match in matches:  # Merging new matches result with the old ones
             match_found = False
             for prev_match_row in prev_matches_rows:
-                if prev_match_row.startswith(";".join(match[:2])):
+                if prev_match_row.startswith(",".join(match[:2])):
                     writer.writerow(
-                        list(match[:3]) + list(prev_match_row.split(";")[3:]))
+                        list(match[:3]) + list(prev_match_row.split(",")[3:]))
                     match_found = True
             if not match_found:
                 writer.writerow(match)
-    done()
+
+    if args.offline == False:
+        log("Uploading matches...", end=" ")
+        client.import_csv(sheets_id["__matches"], open(
+            './matches.csv', 'r', encoding="utf-8").read())
+        done()
 
     log("Finished.")
 
